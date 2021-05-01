8d8a42a2c4bb9b2cfae5ab608cc41300f3f02b1e
Byron Dunkley <ByronDunkley@Shans-MacBook-Pro-2.local>
2021-05-01 23:11:51 +0100

    verify password server route

--- UI/src/index.js
+++ UI/src/index.js
@@ -1,5 +1,7 @@
+import https from 'https';
 import { fastify } from "fastify";
 import fastifyStatic from "fastify-static";
+import fetch from "cross-fetch"
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -12,6 +14,39 @@ async function startApp() {
     try {
         app.register(fastifyStatic, {
             root: path.join(__dirname, "public")
+        })
+
+        app.get("/verify/:email/:token", {}, async (request, reply) => {
+            try {
+                const { email, token }  = request.params;
+
+                const values = {
+                    email,
+                    token
+                }
+            const httpsAgent = new https.Agent({
+                rejectUnauthorized: false
+            })
+
+            const res = await fetch('https://api.nodeauth.dev/api/verify', {
+                        method: "POST",
+                        credentials: "include",
+                        agent: httpsAgent,
+                        body: JSON.stringify(values),
+                        headers: {"Content-type": "application/json; charset=UTF-8"}
+                    })
+
+            console.log(res);
+            reply.code(200).send("All is good!")
+                
+            } catch (error) {
+                console.error(error);
+                reply.send({
+                    data: {
+                        status: "FAILED"
+                    }
+                })
+            }
         })
         
         const PORT = 5000;
